2c4c7f64de3cb8dbfafca6860ca8b6b577abc7d3
ThanuraSilva <thanurasilva1989@gmail.com>
2026-06-21 10:44:15 +0530

    Create user and get selected user are defined

--- src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/controller/UserController.java
+++ src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/controller/UserController.java
@@ -1,18 +1,25 @@
 package lk.ijse.cmjd114_115.EcoCheck2026.controller;
 
+import lk.ijse.cmjd114_115.EcoCheck2026.dto.UserDTO;
+import lk.ijse.cmjd114_115.EcoCheck2026.dto.enums.Role;
 import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/v1/users")
 @RestController
 public class UserController {
-    @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody ??) {
-
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createUser(@RequestBody UserDTO userDTO) {
+        System.out.println("createUser: " + userDTO.toString());
+        return new  ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping(name = "{userId}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> getSelectedUser(@PathVariable String userId) {
+        System.out.println("Selected User: " + userId);
+        return new  ResponseEntity<>(new UserDTO("U001","Kamal","Silva","kamal@mail.com","pw1111", Role.ADMIN), HttpStatus.OK);
     }
 }
