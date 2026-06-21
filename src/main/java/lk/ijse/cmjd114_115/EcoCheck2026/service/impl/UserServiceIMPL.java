b634bf9591724696054b6be04a1ea8ecf671e0a3
ThanuraSilva <thanurasilva1989@gmail.com>
2026-06-21 13:43:29 +0530

    Implemented UserService to the UserServiceIMPL

---
+++ src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/service/impl/UserServiceIMPL.java
@@ -0,0 +1,34 @@
+package lk.ijse.cmjd114_115.EcoCheck2026.service.impl;
+
+import lk.ijse.cmjd114_115.EcoCheck2026.dto.UserDTO;
+import lk.ijse.cmjd114_115.EcoCheck2026.service.UserService;
+
+import java.util.List;
+
+public class UserServiceIMPL implements UserService {
+
+    @Override
+    public void saveUser(UserDTO user) {
+
+    }
+
+    @Override
+    public UserDTO getSelectedUser(String userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public void updateUser(String userId, UserDTO user) {
+
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+}
