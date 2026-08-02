b7b9003a784d1ce670d824daefc06234c0062edb
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-02 11:46:18 +0530

    Add CORS Config

---
+++ src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/config/CORSConfig.java
@@ -0,0 +1,30 @@
+package lk.ijse.cmjd114_115.EcoCheck2026.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+public class CORSConfig {
+    @Bean
+    public FilterRegistrationBean <CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfig.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS","HEAD"));
+        corsConfig.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setExposedHeaders(List.of("Authorization","Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        FilterRegistrationBean<CorsFilter> corsFilterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        corsFilterBean.setOrder(0);
+        return  corsFilterBean;
+
+
+    }
+}
