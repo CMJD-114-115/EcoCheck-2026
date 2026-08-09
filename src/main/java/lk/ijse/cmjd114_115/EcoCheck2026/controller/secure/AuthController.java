ced58ed10a143def9b64d49148628744e2013d52
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-09 12:43:19 +0530

    Minor Correction - AuthController

--- src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/controller/secure/AuthController.java
+++ src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/controller/secure/AuthController.java
@@ -3,6 +3,8 @@ package lk.ijse.cmjd114_115.EcoCheck2026.controller.secure;
 import lk.ijse.cmjd114_115.EcoCheck2026.dto.secure.JWTResponseDTO;
 import lk.ijse.cmjd114_115.EcoCheck2026.dto.UserDTO;
 import lk.ijse.cmjd114_115.EcoCheck2026.dto.secure.SignInDTO;
+import lk.ijse.cmjd114_115.EcoCheck2026.service.secure.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,15 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<JWTResponseDTO> signUp(@RequestBody UserDTO user){
-        return null;
+    public ResponseEntity<JWTResponseDTO> signUp(@RequestBody UserDTO signInUser){
+        return ResponseEntity.ok(authService.signUp(signInUser));
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<JWTResponseDTO> signIn(@RequestBody SignInDTO signInDTOUser){
-        return null;
+    @PostMapping("/signin")
+    public ResponseEntity<JWTResponseDTO> signIn(@RequestBody SignInDTO signUpUser){
+        return ResponseEntity.ok(authService.signIn(signUpUser));
     }
 }
