8a3996bb2a7403ca02d51bfd6f3f5393cb69212e
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-09 10:25:09 +0530

    Modify UserEntity

--- src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/entities/UserEntity.java
+++ src/main/java/lk/ijse/cmjd114_115/EcoCheck2026/entities/UserEntity.java
@@ -3,21 +3,24 @@ package lk.ijse.cmjd114_115.EcoCheck2026.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lk.ijse.cmjd114_115.EcoCheck2026.dto.enums.Role;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Entity
 @Builder
 @Table(name = "users")
-public class UserEntity implements Serializable {
+public class UserEntity implements Serializable, UserDetails {
+
     @Id
     private String userId;
     @Column(nullable = false)
@@ -32,10 +35,21 @@ public class UserEntity implements Serializable {
 
     @OneToMany(mappedBy = "user")
     @JsonIgnore
+    @ToString.Exclude
     private List<UserActionEntity> userActions;
 
     @OneToMany(mappedBy = "user")
     @JsonIgnore
+    @ToString.Exclude
     private List<GoalEntity> goals;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
